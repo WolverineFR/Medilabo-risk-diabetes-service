f6f16ac2fd15d1b867552268a48d0bf30f74be82
Maxence <144704028+WolverineFR@users.noreply.github.com>
2025-10-03 11:08:24 +0200

    first commit

---
+++ src/main/java/com/openclassrooms/medilabo/risk_diabetes_service/RiskDiabetesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilabo.risk_diabetes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskDiabetesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskDiabetesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/medilabo/risk_diabetes_service/RiskDiabetesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilabo.risk_diabetes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskDiabetesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
